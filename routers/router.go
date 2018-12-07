3a8159ce8fbe8beaa1d29c272e6b469f06e3758a
suhanyujie <suhanyujie@qq.com>
2018-12-07 09:45:53 +0800

    访问根路径也是访问文章首页

--- routers/router.go
+++ routers/router.go
@@ -1,12 +1,13 @@
 package routers
 
 import (
-	"beegoBlog/controllers"
 	"github.com/astaxie/beego"
 	"beegoBlog/controllers/article/articleController"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+    //beego.Router("/", &controllers.MainController{})
+    //访问根路径也是访问文章首页
+    beego.Router("/", &articleController.ArticleController{})
     beego.Router("/article", &articleController.ArticleController{})
 }
